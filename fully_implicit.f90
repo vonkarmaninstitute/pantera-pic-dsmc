98026739881e54360a085035d5d69caf62a39a63
Pietro Parodi <pietroparodi01@gmail.com>
2024-02-06 16:03:37 +0100

    (PP) Bugfix allocation of part_adv.

--- fully_implicit.f90
+++ fully_implicit.f90
@@ -884,13 +884,13 @@ MODULE fully_implicit
       CALL COMPUTE_E_FIELD
 
       !  Advect the particles using the guessed new potential
-      ALLOCATE(part_adv, SOURCE = particles)
+      IF (ALLOCATED(particles)) ALLOCATE(part_adv, SOURCE = particles)
       CALL TIMER_START(3)
       CALL ADVECT_CN_B(part_adv, .FALSE., .TRUE., Jmat)
       CALL TIMER_STOP(3)
       CALL DEPOSIT_CHARGE(part_adv)
       IF (JACOBIAN_TYPE == 4) CALL COMPUTE_MASS_MATRICES(part_adv)
-      DEALLOCATE(part_adv)
+      IF (ALLOCATED(part_adv)) DEALLOCATE(part_adv)
 
       ! Compute the new potential from the charge distribution
       !CALL SOLVE_POISSON
